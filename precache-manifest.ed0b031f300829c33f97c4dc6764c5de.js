257206ef650ef81f1ac1caa8cbf0d8e24943dfbf
Mohamed A <102352578+Mohamedro5@users.noreply.github.com>
2022-11-24 18:21:04 +0530

    Add files via upload

---
+++ precache-manifest.ed0b031f300829c33f97c4dc6764c5de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6c0c6a01b0918297bc9",
+    "url": "/static/css/main.dd60ca33.chunk.css"
+  },
+  {
+    "revision": "e6c0c6a01b0918297bc9",
+    "url": "/static/js/main.e6c0c6a0.chunk.js"
+  },
+  {
+    "revision": "9a706a69ef0a8c3185a3",
+    "url": "/static/js/1.9a706a69.chunk.js"
+  },
+  {
+    "revision": "f69ebc39426a191e575a",
+    "url": "/static/js/2.f69ebc39.chunk.js"
+  },
+  {
+    "revision": "ef923b0091c2a93bc9f2",
+    "url": "/static/js/runtime~main.ef923b00.js"
+  },
+  {
+    "revision": "10fbb579e9ce164895f57ecba38fc4a2",
+    "url": "/index.html"
+  }
+];
